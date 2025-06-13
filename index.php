680325edb0a212ef47782ed0d1b3cc7a373e332e
SanketMedhePawar <sanketmedhe311@gmail.com>
2025-06-13 13:15:23 +0530

    updated with html table

--- index.php
+++ index.php
@@ -10,6 +10,30 @@
         <h1>Hello from Sanket Suresh Medhe Pawar!</h1>
         <p>PHP version: <?php echo phpversion(); ?></p>
         <p>Server Time: <?php echo date("Y-m-d H:i:s"); ?></p>
+        <h2>User Table</h2>
+        <table>
+            <thead>
+                <tr>
+                    <th>Name</th>
+                    <th>Age</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>Sanket</td>
+                    <td>25</td>
+                </tr>
+                <tr>
+                    <td>Sai</td>
+                    <td>24</td>
+                </tr>
+                <tr>
+                    <td>Vedant</td>
+                    <td>26</td>
+                </tr>
+            </tbody>
+        </table>
     </div>
+
 </body>
 </html>
