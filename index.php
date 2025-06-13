fb616142221bd68b543b490c3beed135b415fafc
SanketMedhePawar <sanketmedhe311@gmail.com>
2025-06-13 12:56:58 +0530

    initial check in of my phpweb code

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>AWS Beanstalk Test</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="container">
+        <h1>Hello from Sanket Suresh Medhe Pawar!</h1>
+        <p>PHP version: <?php echo phpversion(); ?></p>
+        <p>Server Time: <?php echo date("Y-m-d H:i:s"); ?></p>
+    </div>
+</body>
+</html>
